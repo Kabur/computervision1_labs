210868c83cba6ef72e66af1e13eca0e6ee967495
Kabur <tomasfabry1@gmail.com>
2018-03-06 12:13:03 +0100

    -

--- Assignment_3/main.m
+++ Assignment_3/main.m
@@ -1,25 +1,37 @@
-% *************** Harris Corner Detector demo ****************
-I = imread('boi.jpg');
-[H, c, r] = harris_corner_detector(I);
-figure1 = figure;
-imshow(I)
-hold on;
-plot(c, r, 'ro', 'LineWidth', 2, 'MarkerSize',15)
+% % *************** Harris Corner Detector demo ****************
+% I = imread('boi.jpg');
+% [H, c, r] = harris_corner_detector(I);
+% figure1 = figure;
+% imshow(I)
+% hold on;
+% plot(c, r, 'ro', 'LineWidth', 2, 'MarkerSize',15)
+% 
+% % *************** Lucas Kanade demo ****************
+% I1 = imread('synth1.pgm');
+% I2 = imread('synth2.pgm');
+% [height, width] = size(I2);
+% 
+% window_size = 15;
+% 
+% optical_flow = lucas_kanade(I1, I2, window_size);
+% fig2 = figure;
+% imshow(I2)
+% hold on
+% 
+% center_pos = ceil(window_size/2);
+% [x,y] = meshgrid(center_pos:window_size:width - center_pos, center_pos:window_size:height - center_pos);
+% quiver(x, y, optical_flow(:, :, 1), optical_flow(:, :, 2));
 
-% *************** Lucas Kanade demo ****************
-I1 = imread('synth1.pgm');
-I2 = imread('synth2.pgm');
-[height, width] = size(I2);
+% *************** Feature Tracking demo ****************
+imgPath = './test_delete_later/'; 
+dCell = dir([imgPath '*.jpg']);
 
-window_size = 15;
+for d = 1:length(dCell)
+    image_list{d} = imread([imgPath dCell(d).name]);
+end
 
-optical_flow = lucas_kanade(I1, I2, window_size);
-fig2 = figure;
-imshow(I2)
-hold on
+tracking(image_list);
 
-center_pos = ceil(window_size/2);
-[x,y] = meshgrid(center_pos:window_size:width - center_pos, center_pos:window_size:height - center_pos);
-quiver(x, y, optical_flow(:, :, 1), optical_flow(:, :, 2));
 
-% *************** Feature Tracking demo(eventually xD) ****************
+
+
---
+++ Assignment_3/tracking.m
@@ -0,0 +1,11 @@
+function [] = tracking(image_list)
+
+% fig3 = figure;
+% subplot(121)
+% imshow(image_list{1});
+% subplot(122)
+% imshow(image_list{2});
+size(image_list)
+
+
+end
